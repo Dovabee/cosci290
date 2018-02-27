9437cdfc8ded480ef4261cc758282d20583398a6
kozanshin1994.hack <kozanshin1994.hack@gmail.com>
2018-02-27 02:29:16 -0500

    pg. 75-88, only worked on Test.java, still don't understand what to type.

--- Test.java
+++ Test.java
@@ -1,6 +1,14 @@
 public class Test {
   public static void main(String[] args) {
-    int i = k + 2;
-    System.out.println(i);
+    java.util.Scanner input = new java.util.Scanner(System.in);
+    double x = input.nextDouble();
+    double y = input.nextDouble();
+    double z = input.nextDouble();
+    
+    System.out.println("(x < y && y < z) is " + (x < y && y < z));
+    System.out.println("(x < y || y < z) is " + (x < y || y < z));
+    System.out.println("!(x < y) is " + !(x < y));
+    System.out.println("(x + y < z) is " + (x + y < z));
+    System.out.println("(x + y > z) is " + (x + y > z));
   }
 }
