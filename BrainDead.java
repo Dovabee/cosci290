882bc360e5508d0612b2b02bc49e06eb59dba282
kozanshin1994.hack <kozanshin1994.hack@gmail.com>
2018-03-02 15:58:48 -0500

    Worked on project

--- BrainDead.java
+++ BrainDead.java
@@ -8,6 +8,8 @@ public class BrainDead{
   public static void main(String[] args){
    
     Scanner input = new Scanner(System.in); //Scanner initialized
+    String response = "";
+    String action = "";
         
         //splash screen
         System.out.println("  :::====  :::====  :::====  ::: :::= === :::====  :::===== :::====  :::====  \n "
@@ -26,8 +28,10 @@ public class BrainDead{
     //Gameplay starts
     System.out.println("Eat breakfast or head to the docks? ");
     
+    System.out.println(" ");
+    
     //Player gives response to question
-    String response = input.next();
+    response = input.next();
   
     if((response.toLowerCase()).equals("breakfast")){
       System.out.println("Let's go downstairs to the kitchen.");
@@ -44,7 +48,7 @@ public class BrainDead{
       System.out.println("Run or call for help? ");
       
       //Player decides either to run or call for help
-      String action = input.next();
+      action = input.next();
       
       //1st nested if and else if
       if((action.toLowerCase()).equals("run")){
@@ -56,16 +60,41 @@ public class BrainDead{
       
     }
     else if((response.toLowerCase()).equals("docks")){
-      System.out.println("Call an Uber");
+      
+      System.out.println(" ");
+      
+      System.out.println("You take out your phone and call an Uber");
+      
+      System.out.println(" ");
+      
+      System.out.println("You patiently wait for the Uber to come no matter how long it takes because "
+                       + "you don't want to pay the cancellation fee.");
+      
+       System.out.println("You eventually die of thirst. The end! ");
+                        
+      
+     System.out.println("  ____    ______           ____        _____   __  __  ____    ____    __                                      \n"
+                      + " /\\  _`\\ /\\  _  \\  /'\\_/`\\/\\  _`\\     /\\  __`\\/\\ \\/\\ \\/\\  _`\\ /\\  _`\\ /\\ \\                      \n"
+                      + " \\ \\ \\L\\_\\ \\ \\L\\ \\/\\      \\ \\ \\L\\_\\   \\ \\ \\/\\ \\ \\ \\ \\ \\ \\ \\L\\_\\ \\ \\L\\ \\ \\ \\        \n"
+                      + "  \\ \\ \\L_L\\ \\  __ \\ \\ \\__\\ \\ \\  _\\L    \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  _\\L\\ \\ ,  /\\ \\ \\            \n"
+                      + "   \\ \\ \\/, \\ \\ \\/\\ \\ \\ \\_/\\ \\ \\ \\L\\ \\   \\ \\ \\_\\ \\ \\ \\_/ \\ \\ \\L\\ \\ \\ \\\\ \\\\ \\_\\    \n"
+                      + "    \\ \\____/\\ \\_\\ \\_\\ \\_\\\\ \\_\\ \\____/    \\ \\_____\\ `\\___/\\ \\____/\\ \\_\\ \\_\\/\\_\\            \n"
+                      + "     \\/___/  \\/_/\\/_/\\/_/ \\/_/\\/___/      \\/_____/`\\/__/  \\/___/  \\/_/\\/ /\\/_/                          \n");
     }
     else{
-      System.out.println("I don't understand that ");
+      System.out.println("I don't understand that ");//remember to add loop later
     }
+    
+    
     
     
   
     }
   }
+
+//splash screen
+
+
 
 
 
