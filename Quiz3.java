68e6097519b3462a2e78848334ab254101576739
kozanshin1994.hack <kozanshin1994.hack@gmail.com>
2018-03-08 17:34:58 -0500

    turned in quiz late

---
+++ Quiz3.java
@@ -0,0 +1,75 @@
+/*
+  Francisco Delgado
+  
+  Co Sci 290
+  
+  Group 1 Quiz#3b
+*/
+
+//importing scanner package from java
+import java.util.Scanner;
+
+public class Quiz3{
+  
+  //beginning problem 1.9
+  public static void main(String[] args){
+    
+    //initializing scanner for inputs
+    Scanner input = new Scanner(System.in);
+    
+    //variable declarations
+        double width = 4.5;
+       double height = 7.9;
+    
+    //compute area
+    double area = (4.5 * 7.9);
+    
+    //compute perimeter
+    double perimeter = (2.0 * 4.5) + (2.0 * 7.9);
+    
+    //display results
+    System.out.println("The area of a rectangle with a width of " + width + " and a height of " + height + 
+                      " is " + area);
+    
+    System.out.println(" ");
+    
+    System.out.println("The perimeter of a rectangle with a width of " + width + " and a height of " + height + 
+                      " is " + perimeter);
+    
+    System.out.println(" ");
+    
+    //Beginning problem 1.10
+    
+    //variable declarations for problem 1.10
+    
+    
+    
+    System.out.println("The average speed in miles per hour of a runner that runs 14 kilometers in 45 minutes " + 
+                      " is " );
+    
+  }
+}
+//14 kilometers in miles is 22.4 miles
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
