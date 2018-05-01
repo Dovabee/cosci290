773c1aa84f346826411272b7a968d579b8ac8532
kozanshin1994.hack <kozanshin1994.hack@gmail.com>
2018-05-01 19:35:04 -0400

    Trying to test loops... it didn't work out.

---
+++ TestLoops.java
@@ -0,0 +1,55 @@
+public static void main(String[] args) {
+  
+  public class TestLoops {
+    
+    //declare "count" AND initialize it by making it equal zero
+    int count = 0;
+    while (count < 100) {
+      System.out.println("Welcome to Java!");
+      count++;
+    }
+    
+  }
+}
+
+/*
+  While Loop notes
+  ----------------------------------------
+  
+  - A "while loop" executes statements repeatedly while the condition is true
+                                                  ```````````````````````````
+  - A method is a construct that contains statements
+  
+  - This is an example of a statement:----------
+                                       ________|_________
+                                      |System.out.println|
+                                       ------------------
+  - "System.out.println" is a statement
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+*/
+
+
+
+
+
+
+
+
+
+
+
