e80d21ea51818b20c796c30131b53810a7f7c44f
kozanshin1994.hack <kozanshin1994.hack@gmail.com>
2018-05-03 18:46:21 -0400

    Still working on Lab18

---
+++ Lab18.java
@@ -0,0 +1,139 @@
+/*
+  Francisco Delgado
+  
+  Lab 18
+  
+  Co Sci 290
+  
+  5.10 (Find numbers divisible by 5 and 6)
+  Write a program that displays all the num-
+  bers from 100 to 1,000, ten per line, that are divisible by 5 and 6. Numbers are
+  separated by exactly one space
+  ------------------------------------------------------------------------------------------------
+  5.16 (Find the factors of an integer)
+  Write a program that reads an integer and displays all its smallest factors in increasing order.
+  For example, if the input integer is 120, the output should be as follows: 2, 2, 2, 3 , 5 
+*/
+import java.util.Scanner
+
+public class Lab18{
+  
+  //program execution
+  public static void main(String[] args){
+  
+  Scanner input = new Scanner (System.in);
+    
+    /*
+    
+      BEGINNING PROBLEM 5.10
+      
+    */
+    
+    int counter = 0;
+    
+    for(int i = 100; i <= 1000;i+=10){
+      
+      if(i % 5 == 0 || i % 6 == 0){
+        counter ++;
+        System.out.print(i + " ");
+      }
+      
+      if(counter % 10 == 0){
+        System.out.println();
+      }
+      
+    }//end of for loop
+    
+    //END OF PROBLEM 5.10
+    System.out.println(" ");
+    System.out.println("End of problem 5.10 ");
+    
+    /*
+    
+      BEGINNING PROBLEM 5.16
+    
+    */
+    
+    //Declared variables
+    //final int NumOfPrimes = 11;
+    //final int PrimesPerLine = 10;
+    //int count = 0; //count the number of prime numbers
+    //int number = 2; //
+    int number;
+    
+    //prompt user to enter an integer
+    System.out.println("Enter an integer: ");
+    number = input.nextInt;
+    
+    //example: find the factors of 18
+    if(number % 2 == 0){
+    System.out.println(" ");
+    
+    }
+    
+    
+  }//end of main
+}//end of class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- TestLoops.java
+++ TestLoops.java
@@ -1,16 +1,103 @@
-public static void main(String[] args) {
+/*
+  Author: Francisco
   
-  public class TestLoops {
+  Purpose: Play around with loops
+*/
+
+public class TestLoops{
+
+   //entry point of program
+   public static void main(String[] args){
+     
+     int counter = 0;
+     
+     for(int i = 1; i <= 100;i++){ //i++ is the same as i = i + 1
+       //i+=2 is the same as i = i + 2
+       
+       //i++ means to increment by one
+       //i+=2 means to increment the value of i by 2
+       
+       if(i % 3 == 0){
+        counter++;
+        System.out.print(i + " ");
+       }
+       
+       if(counter % 5 == 0){
+         System.out.println();
+       }
+     }
     
-    //declare "count" AND initialize it by making it equal zero
-    int count = 0;
-    while (count < 100) {
-      System.out.println("Welcome to Java!");
-      count++;
-    }
-    
-  }
-}
+     
+   }//end of main
+}//end of class
+
+
+
+
+
+
+
+
+
+/*
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
   While Loop notes
