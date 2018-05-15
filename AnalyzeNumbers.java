33988df77ec12b62522b7de7e4ba018a4ebd48d3
kozanshin1994.hack <kozanshin1994.hack@gmail.com>
2018-05-15 18:22:44 -0400

    Some random stuff

---
+++ AnalyzeNumbers.java
@@ -0,0 +1,18 @@
+/*
+  This is from the book on pg. 255
+  
+*/
+
+public class AnalyzeNumbers{
+  
+  //entry point of program
+  public static void main(String [] args){
+    
+    java.util.Scanner input = new java.util.Scanner(System.in);
+    
+    System.out.print("Enter the number of items: ");
+    int n = input.nextInt();
+    double[] numbers = new double[n]
+    
+  }
+}
