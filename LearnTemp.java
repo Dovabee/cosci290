60bdb4feb314a766231b53eb4d80c46bb982c1cf
kozanshin1994.hack <kozanshin1994.hack@gmail.com>
2018-05-22 19:59:05 -0400

    Added a new file to learn temp for lab22.

---
+++ LearnTemp.java
@@ -0,0 +1,86 @@
+/*
+  
+  Learn how to transfer values between variables!!!
+  
+*/
+import java.util.Scanner;
+
+public class LearnTemp {
+  
+  //Start of program
+  public static void main(String[] args) {
+    
+    Scanner input = new Scanner(System.in);
+    
+    //declare variables
+    int number1;
+    int number2;
+    int temp;
+    int answer;
+    
+    //1. generate two random single-digit integers
+    number1 = (int)(Math.random() * 10);
+    number2 = (int)(Math.random() * 10);
+    
+    //2. If number1 < number2, swap number1 with number2
+    if(number1 < number2) {
+      temp = number1;
+      number1 = number2;
+      number2 =temp;
+    }
+    
+    //3. Prompt the student to answer "What is number1 - number2?"
+    System.out.println("What is " + number1 + " - " + number2 + "?");
+    answer = input.nextInt();
+    
+    //4. Grade the answer and display the result
+    if(number1 - number2 == answer)
+      System.out.println("You are correct!");
+      else {
+        System.out.println("Your answer is wrong.");
+        System.out.println(" ");
+        System.out.println(number1 + " - " + number2 + " should be " + (number1 - number2));
+      }
+      
+  }//end of main
+}//end of class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
