0f08235e287f315f9b0820feda49340b074782ca
kozanshin1994.hack <kozanshin1994.hack@gmail.com>
2018-05-24 18:36:43 -0400

    Found the min number for Lab 22

--- Lab22.java
+++ Lab22.java
@@ -49,17 +49,17 @@ public class Lab22 {
     System.out.println("The largest number is " + max + ".");
     
     
-    max = counts[0];
+    int min = counts[0];
     //declaring variable
     int indexOfMax = 0;
     
     //this for loop finds the smallest element in the "counts" array.
     for(int i = 1; i < counts.length; i++){
-      if(counts[i] < max){
-        max = counts[i];
+      if(counts[i] < min){
+        min = counts[i];
         indexOfMax = i;
         System.out.println(" ");
-    System.out.println("The smallest number is " + indexOfMax + ".");
+    System.out.println("The smallest number is " + counts[i] + ".");
       }
     }//end of for loop
     
